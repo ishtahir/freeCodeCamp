58d2961aa0ee47f139069a1538980921b9d00030
ishtahir <36302474+ishtahir@users.noreply.github.com>
2018-02-09 16:36:08 -0600

    Update 08 - repeatString.js

--- 08 - repeatString.js
+++ 08 - repeatString.js
@@ -1,3 +1,6 @@
+// this function accepts a string and a number, and it simply repeats the string that number of times
+// if number is not positive it returns an empty string
+
 function repeatStringNumTimes(str, num) {
 	if (num > 0) {
 		var newStr = "";
