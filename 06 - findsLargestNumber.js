5b7c318f496f45117b2bc5a98a23b675f17efe5e
ishtahir <36302474+ishtahir@users.noreply.github.com>
2018-02-09 16:33:46 -0600

    Update 06 - findsLargestNumber.js

--- 06 - findsLargestNumber.js
+++ 06 - findsLargestNumber.js
@@ -1,3 +1,5 @@
+// this function accepts a 2D array of four numbers in each inner array, then returns an array of the biggest number in each of the inner arrays
+
 function largestOfFour(arr) {
 	var bigNum = [];
 	var largest = 0;
