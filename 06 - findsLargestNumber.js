158f5886ca0c41e6a8e39eb99c5b8060a39487c2
ishtahir <36302474+ishtahir@users.noreply.github.com>
2018-02-09 09:28:41 -0600

    Create 06 - findsLargestNumber.js

---
+++ 06 - findsLargestNumber.js
@@ -0,0 +1,14 @@
+function largestOfFour(arr) {
+	var bigNum = [];
+	var largest = 0;
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = 0; j < arr[i].length; j++) {
+			if (arr[i][j] > largest) {
+				largest = arr[i][j];
+			}
+		}
+		bigNum.push(largest);
+        largest = 0;
+	}
+	return bigNum;
+}
