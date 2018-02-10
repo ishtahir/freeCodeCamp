7372d50fcb8a8992b3d0159ccf4088aef11b6613
ishtahir <36302474+ishtahir@users.noreply.github.com>
2018-02-10 17:37:30 -0600

    Create 16 - caesarsCipher.js

---
+++ 16 - caesarsCipher.js
@@ -0,0 +1,26 @@
+// this function takes in a string and deciphers it or ciphers it depending on how it came in to the rot13 method
+
+function rot13(str) {
+	str = str.toUpperCase();
+	var decipher = "";
+    var charNum;
+    for (var i = 0; i < str.length; i++) {
+    	charNum = str.charCodeAt(i);
+    	if (charNum === 32) {
+    		decipher += String.fromCharCode(charNum);
+    	}
+    	else if(charNum >=0 && charNum <= 64) {
+    		decipher += String.fromCharCode(charNum);
+    	}
+    	else if (charNum + 13 >= 65 && charNum + 13 <= 90) {
+    		decipher += String.fromCharCode(charNum + 13);
+    	}
+    	else if (charNum - 13 >= 65 && charNum - 13 <= 90) {
+    		decipher += String.fromCharCode(charNum - 13);
+    	}
+    	else {
+    		decipher += String.fromCharCode(charNum);
+    	}
+    }
+    return decipher;
+}
