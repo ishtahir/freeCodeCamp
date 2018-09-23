9b0cc0b5c68ec0d170cfac229854dd7f71c69bcc
Ish Tahir <tahirismaeel@gmail.com>
2018-09-22 20:30:15 -0500

    Updated to ES6 syntax, used built in methods

--- 01 - reverseString.js
+++ 01 - reverseString.js
@@ -1,9 +1,3 @@
 // this function accepts a string value and returns it reversed
 
-function reverseString(str) {
-	var reverse = "";
-	for (var i = (str.length - 1); i >= 0; i--) {
-    	reverse += str[i];
-	}
-	return reverse;
-}
+reverseString = str => str = str.split('').reverse().join('');
