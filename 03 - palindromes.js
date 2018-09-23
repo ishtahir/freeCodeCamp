74b7b56934040ec9f74413482dbfaffa5ffe47b6
Ish Tahir <tahirismaeel@gmail.com>
2018-09-23 18:00:01 -0500

    Added const for variable

--- 03 - palindromes.js
+++ 03 - palindromes.js
@@ -1,6 +1,6 @@
 // this function accepts a string and checks to see if the string entered is a palindrome, returns T or F accordingly
 
 palindrome = str =>	{
-  reverse = str.split('').reverse().join('');
+  const reverse = str.split('').reverse().join('');
   return reverse === str;
 }
