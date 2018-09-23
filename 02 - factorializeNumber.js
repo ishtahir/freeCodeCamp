7950f09dd350d4c0e8b1c541a507f175e4bcab51
Ish Tahir <tahirismaeel@gmail.com>
2018-09-22 20:35:52 -0500

    Simplified code and updated to ES6

--- 02 - factorializeNumber.js
+++ 02 - factorializeNumber.js
@@ -1,14 +1,10 @@
 // this function accepts a number and returns the result of the factorial for that number
 // ex: factorialize(5) = 120 because 5! = 5 * 4 * 3 * 2 * 1
 
-function factorialize(num) {
-	var factorsArray = [];
-	for (var i = 1; i <= num; i++) {
-		factorsArray.push(i);
-	}
-	var factorial = 1;
-	for (var j = 0; j < factorsArray.length; j++) {
-		factorial *= factorsArray[j];
-	}
-	return factorial;
+factorialize = num => {
+  let factor = 1;
+  for (let i = num; i > 0; i--) {
+    factor *= i;
+  }
+  return factor;
 }
