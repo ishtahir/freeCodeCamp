4e5b7c1af3bd479cf42c290e061d88bfca9350fb
Ish Tahir <tahirismaeel@gmail.com>
2018-09-22 20:44:10 -0500

    Refactored the hell out of this code, ES6 syntax

--- 03 - palindromes.js
+++ 03 - palindromes.js
@@ -1,32 +1,6 @@
 // this function accepts a string and checks to see if the string entered is a palindrome, returns T or F accordingly
 
-function palindrome(str) {
-	var strArray = [];
-	var newStr = str.replace(/\s+/g, ' ').replace(/[^a-zA-Z-0-9]/g, '').toLowerCase();
-    	newStr = newStr.replace(/-/g, '');
-	for (var i = 0; i < newStr.length; i++) {
-		strArray.push(newStr[i]);
-	}
-	var reverseArr = [];
-	for (var k = strArray.length - 1; k >= 0; k--) {
-		reverseArr.push(strArray[k]);
-	}
-	console.log(strArray);
-	console.log(reverseArr);
-	var equality = false;
-	for (var j = 0; j < strArray.length; j++) {
-		if (strArray[j] === reverseArr[j]) {
-			equality = true;
-		}
-		else {
-			equality = false;
-            break;
-		}
-	}
-	if (equality) {
-		return true;
-	}
-	else {
-		return false;
-	}
+palindrome = str =>	{
+  reverse = str.split('').reverse().join('');
+  return reverse === str ? true : false;
 }
