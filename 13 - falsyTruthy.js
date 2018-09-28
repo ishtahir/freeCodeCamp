38260a64eeb5de6f8690ae0a696f24af23f95ed0
Ish Tahir <tahirismaeel@gmail.com>
2018-09-27 22:58:05 -0500

    Refactored code, ES6

--- 13 - falsyTruthy.js
+++ 13 - falsyTruthy.js
@@ -1,5 +1,2 @@
-// this function takes in a array and filters out all the falsy types in javascript and returns the cleaned out array
-
-function bouncer(arr) {
-	return arr.filter(Boolean);
-}
+// this function takes in a array and returns all truthy elements in the array
+bouncer = arr => arr.filter(Boolean);
