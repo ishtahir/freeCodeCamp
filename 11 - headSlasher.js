a230e82859bc285dd4b871e22f82f98217a8bdfe
Ish Tahir <tahirismaeel@gmail.com>
2018-09-27 22:55:04 -0500

    Refactored code, ES6

--- 11 - headSlasher.js
+++ 11 - headSlasher.js
@@ -1,6 +1,7 @@
-// this function takes in an array and a number, then it removes the the number of elements starting at index 0 and returns the remaining array
+// this function takes in an array and a number
+// then it removes that number of elements from the beginning and returns the remaining array
 
-function slasher(arr, howMany) {
+slasher = (arr, howMany) => {
     arr.splice(0, howMany);
 	return arr;
 }
