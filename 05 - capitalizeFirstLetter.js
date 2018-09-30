325af5beaeeeb13432d779eb3f72ebc44d079781
Ish Tahir <tahirismaeel@gmail.com>
2018-09-29 20:34:03 -0500

    Updated to function syntax

--- 05 - capitalizeFirstLetter.js
+++ 05 - capitalizeFirstLetter.js
@@ -1,6 +1,6 @@
 // this function accepts a string and then capitalizes the first letter of every word in the string
 
-titleCase = str => {
+function titleCase(str) {
   const splitStr = str.toLowerCase().split(' ');
   splitStr.forEach((val, i) => {
       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substr(1);
