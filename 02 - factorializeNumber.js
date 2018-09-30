5e72bcf7852ded10f9266d9048923898b2b0a278
Ish Tahir <tahirismaeel@gmail.com>
2018-09-29 20:30:09 -0500

    Updated to function syntax

--- 02 - factorializeNumber.js
+++ 02 - factorializeNumber.js
@@ -1,7 +1,7 @@
 // this function accepts a number and returns the result of the factorial for that number
 // ex: factorialize(5) = 120 because 5! = 5 * 4 * 3 * 2 * 1
 
-factorialize = num => {
+function factorialize(num) {
   let factor = 1;
   for (let i = num; i > 0; i--) {
     factor *= i;
