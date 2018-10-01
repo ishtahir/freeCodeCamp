1b04b8c7daad6d6d47727bab4f1ffea0da30a723
Ish Tahir <tahirismaeel@gmail.com>
2018-09-30 19:03:29 -0500

    Updated to function syntax

--- 08 - repeatString.js
+++ 08 - repeatString.js
@@ -1,6 +1,6 @@
 // repeat a given string num times, return an empty string if num is not a positive number, do not use built in .repeat() method
 
-repeatStringNumTimes = (str, num) => {
+function repeatStringNumTimes(str, num) {
 	if (num > 0) {
 		let newStr = '';
 		for (let i = 0; i < num; i++) {
