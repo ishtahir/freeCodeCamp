4c5400bcad8687d8b496a45234f293779042b22a
Ish Tahir <tahirismaeel@gmail.com>
2018-09-30 19:04:25 -0500

    Updated to function syntax

--- 09 - truncateString.js
+++ 09 - truncateString.js
@@ -1,6 +1,6 @@
 // this function accepts a string and a number, it truncates the string to the number of characters it is provided
 
-truncateString = (str, num) => {
+function truncateString(str, num) {
   if (num < str.length) {
     str = `${str.slice(0, num)}...`;
   }
