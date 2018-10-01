a562c0d81ccf531c404c71bee54a01d9ed5c2dce
Ish Tahir <tahirismaeel@gmail.com>
2018-09-30 19:02:10 -0500

    Updated to function syntax

--- 07 - confirmEnding.js
+++ 07 - confirmEnding.js
@@ -1,10 +1,10 @@
 // this function accepts a string and a target string, it checks if the target is at the very end of the string
 // returns T or F accordingly
 
-confirmEnding = (str, target) => {
+function confirmEnding(str, target) {
 	let newTarget = '';
 	for (let i = (str.length - target.length); i < str.length; i++) {
 		newTarget += str[i];
 	}
-	return newTarget === target ? true : false;
+	return newTarget === target;
 }
