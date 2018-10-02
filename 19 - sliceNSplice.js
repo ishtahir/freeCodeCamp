45caa0864a0fcdc3804e6bccdceead93c74807b1
Ish Tahir <tahirismaeel@gmail.com>
2018-10-01 19:09:54 -0500

    Updated to function syntax

--- 19 - sliceNSplice.js
+++ 19 - sliceNSplice.js
@@ -1,7 +1,7 @@
 // given two arrays and an index, use the array methods slice and splice to copy each element of the first array
 // into the second array, in order, beginning at the index of the second array
 
-frankenSplice = (arr1, arr2, index) => {
+function frankenSplice(arr1, arr2, index) {
   const newArr = arr2.slice();
   newArr.splice(index, 0, ...arr1);
   return newArr;
