d3b38b45607b80a09b5fa21a21dd9d9ef170ce1f
Ish Tahir <tahirismaeel@gmail.com>
2018-10-01 19:08:06 -0500

    Updated to function syntax

--- 11 - headSlasher.js
+++ 11 - headSlasher.js
@@ -1,7 +1,8 @@
 // this function takes in an array and a number
 // then it removes that number of elements from the beginning and returns the remaining array
 
-slasher = (arr, howMany) => {
-    arr.splice(0, howMany);
-	return arr;
+function slasher(arr, howMany) {
+	const newArr = arr.slice();
+	newArr.splice(0, howMany);
+	return newArr;
 }
