d2b48a786f2d066fa9726c9ae4883e5a1fc4e01e
Ish Tahir <tahirismaeel@gmail.com>
2018-10-02 22:53:16 -0500

    Refactor code, ES6

--- 14 - seekAndDestroy.js
+++ 14 - seekAndDestroy.js
@@ -3,8 +3,8 @@
 function destroyer(arr) {
     for (var i = 1; i < arguments.length; i++) {
     	while (arr.includes(arguments[i])) {
-    	var index = arr.indexOf(arguments[i]);
-    	arr.splice(index, 1);
+    	    let index = arr.indexOf(arguments[i]);
+    	    arr.splice(index, 1);
     	}
     }
     return arr;
