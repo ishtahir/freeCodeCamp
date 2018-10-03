3959a9f6a375a42b5a96dab3190fa661cc7fcfe5
Ish Tahir <tahirismaeel@gmail.com>
2018-10-02 20:27:15 -0500

    Refactored code, ES6

--- 12 - mutations.js
+++ 12 - mutations.js
@@ -1,11 +1,10 @@
 // this function takes in an array with 2 elements, returns true if the first element contains all the letters of the second element
 
 function mutation(arr) {
-	var check = false;
-	var newArr = [];
+	let check = false;
+	const newArr = [];
 	newArr.push(arr[0].toLowerCase());
 	newArr.push(arr[1].toLowerCase());
-	console.log(newArr);
 	for (var i = 0; i < newArr[1].length; i++) {
 		if (newArr[0].includes(newArr[1][i])) {
 			check = true;
