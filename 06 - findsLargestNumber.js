c45ae9c623526ad531116fea4dccc6e0847fb756
Ish Tahir <tahirismaeel@gmail.com>
2018-10-04 19:13:12 -0500

    Refactored code, ES6

--- 06 - findsLargestNumber.js
+++ 06 - findsLargestNumber.js
@@ -1,8 +1,8 @@
 // this function accepts a 2D array of four numbers in each inner array, then returns an array of the biggest number in each of the inner arrays
 
 function largestOfFour(arr) {
-	var bigNum = [];
-	var largest = 0;
+	const bigNum = [];
+	let largest = 0;
 	for (var i = 0; i < arr.length; i++) {
 		for (var j = 0; j < arr[i].length; j++) {
 			if (arr[i][j] > largest) {
