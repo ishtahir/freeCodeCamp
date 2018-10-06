9279afc784867702dcfbb2beefcd8d399a8bd810
Ish Tahir <tahirismaeel@gmail.com>
2018-10-05 19:30:59 -0500

    Updated to function syntax

--- 18 - sumNumbersInRange.js
+++ 18 - sumNumbersInRange.js
@@ -1,6 +1,6 @@
 // given an array with two number elements, return the sum of the two numbers plus the sum of all the numbers between them 
 
-sumAll = arr => {
+function sumAll(arr) {
   const myarr = arr.slice().sort((a, b) => a - b);
   let count = 0;
   for (let i = myarr[0]; i <= myarr[1]; i++) {
