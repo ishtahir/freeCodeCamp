b8686318d2f992ecc2533c3b89e3404904769f8c
Ish Tahir <tahirismaeel@gmail.com>
2018-10-09 10:18:53 -0500

    Refactored code, ES6

--- 10 - chunkArrays.js
+++ 10 - chunkArrays.js
@@ -2,9 +2,8 @@
 // of the number and then it returns the 2D array
 
 function chunkArrayInGroups(arr, size) {
-	var newArr = [];
-	var initLength = arr.length / size;
-	console.log("the length: " + (arr.length/size));
+	const newArr = [];
+	const initLength = arr.length / size;
 	for (var i = 0; i < initLength; i++) {
 		newArr.push(arr.slice(0, size));
 		arr.splice(0, size);
